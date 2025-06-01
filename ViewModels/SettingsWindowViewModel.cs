e444877a98c2cf97af320d0205b58addaba80329
AbrLis <abrli@yandex.ru>
2025-06-01 21:48:49 +0500

    Исправление отображения данных в окне настроек при изменении слайдеров.

--- ViewModels/SettingsWindowViewModel.cs
+++ ViewModels/SettingsWindowViewModel.cs
@@ -12,30 +12,55 @@ public class SettingsWindowViewModel : INotifyPropertyChanged
     public double BackgroundOpacity
     {
         get => _mainViewModel.BackgroundOpacity;
-        set => _mainViewModel.BackgroundOpacity = value;
+        set
+        {
+            _mainViewModel.BackgroundOpacity = value;
+            OnPropertyChanged();
+        }
     }
 
     public double TextOpacity
     {
         get => _mainViewModel.TextOpacity;
-        set => _mainViewModel.TextOpacity = value;
+        set
+        {
+            _mainViewModel.TextOpacity = value;
+            OnPropertyChanged();
+        }
     }
 
     public double FontSize
     {
         get => _mainViewModel.FontSize;
-        set => _mainViewModel.FontSize = value;
+        set
+        {
+            _mainViewModel.FontSize = value;
+            OnPropertyChanged();
+        }
     }
 
     public bool ShowSeconds
     {
         get => _mainViewModel.ShowSeconds;
-        set => _mainViewModel.ShowSeconds = value;
+        set
+        {
+            _mainViewModel.ShowSeconds = value;
+            OnPropertyChanged();
+        }
     }
 
     public SettingsWindowViewModel(MainWindowViewModel mainViewModel)
     {
         _mainViewModel = mainViewModel;
+        
+        // Подписываемся на изменения свойств MainWindowViewModel
+        _mainViewModel.PropertyChanged += MainViewModel_PropertyChanged;
+    }
+
+    private void MainViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        // Пробрасываем уведомления об изменениях свойств
+        OnPropertyChanged(e.PropertyName);
     }
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
