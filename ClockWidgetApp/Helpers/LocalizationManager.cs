fdfe2d34b4f9cd6c27caa81292d44ba4dccd20e9
AbrLis <abrli@yandex.ru>
2025-07-01 16:20:45 +0500

    Заготовка окна под таймеры и будильники

--- ClockWidgetApp/Helpers/LocalizationManager.cs
+++ ClockWidgetApp/Helpers/LocalizationManager.cs
@@ -78,4 +78,27 @@ public class LocalizedStrings
     public string LanguageLabel => LocalizationManager.GetString("Language_Label", _lang);
     public string MainWindow_Title => LocalizationManager.GetString("MainWindow_Title", _lang);
     public string AnalogClockWindow_Title => LocalizationManager.GetString("AnalogClockWindow_Title", _lang);
+    public string TimersAndAlarms_Tab => LocalizationManager.GetString("TimersAndAlarms_Tab", _lang);
+    public string Timers_Title => LocalizationManager.GetString("Timers_Title", _lang);
+    public string Timers_Add => LocalizationManager.GetString("Timers_Add", _lang);
+    public string Timers_DisableAll => LocalizationManager.GetString("Timers_DisableAll", _lang);
+    public string Timers_DeleteAll => LocalizationManager.GetString("Timers_DeleteAll", _lang);
+    public string Timers_ActivateAll => LocalizationManager.GetString("Timers_ActivateAll", _lang);
+    public string Timers_Delete => LocalizationManager.GetString("Timers_Delete", _lang);
+    public string Timers_Status => LocalizationManager.GetString("Timers_Status", _lang);
+    public string Timers_Disable => LocalizationManager.GetString("Timers_Disable", _lang);
+    public string Timers_Time => LocalizationManager.GetString("Timers_Time", _lang);
+    public string Timers_Active => LocalizationManager.GetString("Timers_Active", _lang);
+    public string Timers_Inactive => LocalizationManager.GetString("Timers_Inactive", _lang);
+    public string Alarms_Title => LocalizationManager.GetString("Alarms_Title", _lang);
+    public string Alarms_Add => LocalizationManager.GetString("Alarms_Add", _lang);
+    public string Alarms_Delete => LocalizationManager.GetString("Alarms_Delete", _lang);
+    public string Alarms_Status => LocalizationManager.GetString("Alarms_Status", _lang);
+    public string Alarms_Enable => LocalizationManager.GetString("Alarms_Enable", _lang);
+    public string Alarms_Time => LocalizationManager.GetString("Alarms_Time", _lang);
+    public string WidgetSettings_Title => LocalizationManager.GetString("WidgetSettings_Title", _lang);
+    public string WidgetSettings_Size => LocalizationManager.GetString("WidgetSettings_Size", _lang);
+    public string WidgetSettings_SizeTooltip => LocalizationManager.GetString("WidgetSettings_SizeTooltip", _lang);
+    public string WidgetSettings_Topmost => LocalizationManager.GetString("WidgetSettings_Topmost", _lang);
+    public string WidgetSettings_TopmostTooltip => LocalizationManager.GetString("WidgetSettings_TopmostTooltip", _lang);
 } 
