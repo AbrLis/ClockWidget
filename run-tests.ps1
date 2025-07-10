d270bc95af80ee95b699674632e768ec86d49789
AbrLis <abrli@yandex.ru>
2025-07-10 12:01:18 +0500

    скрипт запуска тестов с подробным выводом

---
+++ run-tests.ps1
@@ -0,0 +1,4 @@
+# Скрипт для запуска юнит-тестов проекта ClockWidgetApp
+# Запускает тесты с подробным выводом в консоль
+
+dotnet test CleanTest/CleanTest.csproj --logger "console;verbosity=normal" 
